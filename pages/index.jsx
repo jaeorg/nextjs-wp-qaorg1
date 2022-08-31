acfed4bc100da8dcddcc0a34578371b15ceaf5b2
jaeuser <106764110+jaeuser@users.noreply.github.com>
2022-08-30 19:15:13 -0700

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -15,7 +15,7 @@ export default function Home({ menuItems, posts }) {
           className="text-blue-600 no-underline hover:underline"
           href="https://nextjs.org"
         >
-          Next.js!
+          Next.js! QA
         </a>
       </h1>
 
